b542b99664e342790d6c5e19ee2a5d34e365f315
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2025-01-14 10:03:06 +0530

    new03

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 def add(a, b):
     return a + b
 
+
 def sub(a, b):
     return a-b
 
-def mul(a, b):
-    return a*b
 
+def mul(a, b):
+    return a * b
 
 
 if __name__ == "__main__":
@@ -14,6 +15,3 @@ if __name__ == "__main__":
     result2 = sub(4, 1)
 
     print(result1, result2)
-
-
-    
